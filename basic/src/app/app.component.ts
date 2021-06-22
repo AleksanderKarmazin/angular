8b2b8c97bd73984b105850ac9f41bb6cc6bbf5f5
karmazin <kubartphil@gmail.com>
2021-06-22 23:34:56 +0300

    Profile set up

--- basic/src/app/app.component.ts
+++ basic/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic';
+  text = ' Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+
+  inputValue = ''
+
+  // img = 'https://cdn-media-1.freecodecamp.org/images/xn6oqsiP9ZUwYN98Yidh-Obr7UhkAHHqcFvh'
+
+  constructor(){
+    // setTimeout(() => {
+    //   this.img = 'https://proweb63.ru/assets/chema/images/blog/angular.png'
+    // }, 5000)
+    
+  }
+
+  // onClick(event: any){
+  //   const res = this.inputValue += event.target.value + ' | ';
+  //   console.log("E", res )
+  // }
+  onClick(event: KeyboardEvent){
+    const res = this.inputValue += (<HTMLInputElement> event.target).value + ' | ';
+    console.log("E", res )
+  }
+  onClick2(){
+    console.log("Click 222")
+  }
+
 }
--- basic/src/app/app.module.ts
+++ basic/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { Todo2Component } from './todo2/todo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    Todo2Component
   ],
   imports: [
     BrowserModule,
